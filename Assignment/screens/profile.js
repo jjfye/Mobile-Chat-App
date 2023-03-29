4353ff89a77a3657e8df5a8b7aec377b9952e674
Jason Ye <jye882@gmail.com>
2023-03-29 13:21:49 +0100

    Added Log out button (Fully working)

--- Assignment/screens/profile.js
+++ Assignment/screens/profile.js
@@ -6,16 +6,33 @@ class Profile extends Component{
     header: null
   };
 
+  _onLogOutButton = () => {
+    fetch('http://127.0.0.1:3333/api/1.0.0/logout', {
+      method: 'POST',
+      headers: {
+        'X-Authorization': this.props.token
+      }
+    })
+    .then(response => {
+      console.log('API response:', response);
+      navigation.navigate('Login');
+    })
+    .catch(error => {
+      console.error('API error:', error);
+      // Handle the error from the API server
+      this.setState({ error: "Please try again" });
+    });
+  }
+
   render(){
     return (
-        <View style={styles.container}>
-          <Text style={styles.text}>About</Text>
-		  <TouchableOpacity
-		    style={styles.buttonContainer}
-            onPress={() => this.props.navigation.goBack()}>
-			<Text style={styles.buttonText}>Go to Home</Text>
-		  </TouchableOpacity>
-        </View>
+      <View style={{ flexDirection: "row", justifyContent: "center"}}>
+        <TouchableOpacity
+          style={[styles.btnContainer]}
+          onPress={this._onLogOutButton}>
+          <Text style={styles.buttonText}>log out</Text>
+        </TouchableOpacity>
+      </View> 
     );
   }
 }
@@ -26,6 +43,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: 'violet'
+  },
+  btnContainer: {
+    alignSelf: 'center',
+    alignContent: 'center',
+    backgroundColor: '#222',
+    width: "37%",
+    borderRadius: 10,
+    padding: 12,
+    margin: 20,
   },
   text: {
     color: 'white',
